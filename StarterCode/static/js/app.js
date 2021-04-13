95b5a6f3eb04605d51c67610b4957fda293fa653
Kelly <kmrose08@gmail.com>
2021-04-13 18:54:00 -0400

    updated files

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,61 +1,72 @@
 
-var metadata = data.metadata;
 
-d3.json("/../samples.json").then(function(data){
-        console.log(metadata);
+d3.json("../samples.json").then(function (data) {
+  console.log(data);
 });
 
-//   const dataPromise = d3.json(url);
-//   console.log("Data Promise: ", dataPromise);
-
-
-// "id": 940,
-// "ethnicity": "Caucasian",
-// "gender": "F",
-// "age": 24,
-// "location": "Beaufort/NC",
-// "bbtype": "I",
-// "wfreq": 2
-
-
-// /
-// "id": "940",
-// "otu_ids": [... ],
-// "sample_values": [... ],
-// "otu_labels": [... 
 
 function unpack(rows, index) {
-    return rows.map(function(row) {
-      return row[index];
-    });
-  }
+  return rows.map(function (row) {
+    return row[index];
+  });
+}
 
-    // Grab values from the response json object to build the plots
-    var name = data.dataset.name;
-    var 
-    var stock = data.dataset.dataset_code;
-    var startDate = data.dataset.start_date;
-    var endDate = data.dataset.end_date;
-    var dates = unpack(data.dataset.data, 0);
-    var openingPrices = unpack(data.dataset.data, 1);
-    var highPrices = unpack(data.dataset.data, 2);
-    var lowPrices = unpack(data.dataset.data, 3);
-    var closingPrices = unpack(data.dataset.data, 4);
+// Grab values from the response json object to build the plots
+var name = data.name;
+
+//WHICH WAY IS CORRECT for the other two??
+
+//Method #1
+//read in "metadata" 
+var personId = data.metadata.id
+var ethnicity = data.metadata.ethnicity;
+var gender = data.metadata.gender;
+var age = data.metadata.age;
+var location = data.metadata.location;
+var bbtype = data.metadata.bbtype;
+var wfreq = data.metadata.wfreq;
+//read in "samples"
+var sampleID = data.samples.id;
+var otu_ids = data.samples.otu_ids;
+var sample_values = data.samples.otu_labels;
+
+//Method #2
+//read in "metadata"
+var personId = unpack(data.metadata.id, 0);
+var ethnicity = unpack(data.metadata.ethnicity, 1);
+var gender = unpack(data.metadata.gender, 2);
+var age = unpack(data.metadata.age, 3);
+var location = unpack(data.metadata.age, 4);
+var bbtype = unpack(data.metadata.bbtype, 5);
+var wfreq = unpack(data.metadata.wfreq, 6);
+//read in "samples"
+var sampleID = unpack(data.samples.id, 0);
+var otu_ids = unpack(data.samples.otu_ids, 1);
+var sample_values = unpack(data.samples.otu_labels, 2);
+
+// Create the Trace
+var trace1 = {
+  x: sample_values,
+  y: otu_ids,
+  type: "bar"
+};
+
+// Create the data array for the plot
+var barOtuData = [trace1];
+
+// Define the plot layout
+var layout = {
+  title: "Top 10 OTUs",
+  xaxis: { title: "Count" },
+  yaxis: { title: "OTU" }
+};
+
+var layout = {
+  title: "'Bar' Chart"
+};
 
 
-    var trace1 = {
-        type: "scatter",
-        mode: "lines",
-        name: name,
-        x: dates,
-        y: closingPrices,
-        line: {
-          color: "#17BECF"
-        }
-      }; 
-
-var data = [trace1, trace2];      
-
+// Plot the chart to a div tag with id "bar-plot"
 Plotly.newPlot("bar-plot", data, layout);
 
 // Add event listener for submit button
