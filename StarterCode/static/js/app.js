ea621c70065c90c3bbf1f9ed8e421b4793ddad38
Kelly <kmrose08@gmail.com>
2021-04-28 16:35:35 -0400

    finished files

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -36,70 +36,35 @@ function show() {
     console.log(sample);
 
     var trace1 = {
-      x: sample.sample_values,
-        y: sample.otu_ids,
+      x: sample.otu_ids,
+        y: sample.sample_values,
         type: 'bubble',
         mode: 'markers',
       marker: {
-        size:  sample.otu_ids
+        size: sample.sample_values,
+        color: sample.otu_ids,
+        colorscale: 'Earth'
       }
     };
     
     var data = [trace1];
     
     var layout = {
-      title: 'Marker Size',
+      title: 'Belly Button Bacteria',
       showlegend: false,
-      height: 600,
-      width: 600
+      text: sample.otu_labels,
+      xaxis: {
+        title: "OTU ID"
+      },
+      yaxis: {
+        title: "Sample Value"
+      }
     };
     
-    Plotly.newPlot('bubble', data);
-
-
-    var data = [
-      {
-        x: sample.sample_values,
-        y: sample.otu_ids,
-        type: 'bubble',
-        mode: 'markers',
-          marker: {
-          //  color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
-          // opacity: [1, 0.8, 0.6, 0.4],
-          // size: [40, 60, 80, 100]
-      }}
-    ];
-
-
-  
-
-  // Plotly.newPlot('bubble', data);
+    Plotly.newPlot('bubble', data, layout);
 
 
 
-
-
-    // var trace1 = {
-    //   x: sample.sample_values,
-    //   y: sample.otu_ids,
-    //   mode: 'markers',
-    //   marker: {
-    //     color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
-    //     opacity: [1, 0.8, 0.6, 0.4],
-    //     size: [40, 60, 80, 100]
-    //   }
-    // };
-    
-    // var databubble = [trace1];
-    
-    // var layout = {
-    //   title: 'Marker Size and Color',
-    //   showlegend: false,
-    //   height: 600,
-    //   width: 600
-    // };
-    
-    // Plotly.newPlot('plot', data, layout);
 
 
     
